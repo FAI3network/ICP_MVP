a6403ca3c72fafffc3a9cdaaf28ccab04847765a
Leo <80970758+LeonardoRyuta@users.noreply.github.com>
2025-04-19 14:31:06 +0000

    Added form for fairness tests

--- src/FAI3_frontend/src/components/ui/select.tsx
+++ src/FAI3_frontend/src/components/ui/select.tsx
@@ -23,18 +23,20 @@ export const Select = ({ options, selection, setSelection, multiple = false, pla
     if (multiple) {
       const multipleString = multipleSelection.join(", ");
       setSelection(multipleString);
+      console.log("multipleSelection", multipleSelection);
     }
   }, [multipleSelection]);
 
   useEffect(() => {
     if (selection.length > 0 && multiple) {
       setMultipleSelection(selection.split(", "));
+      console.log("multipleSelection", multipleSelection);
     }
   }, [selection]);
 
   return (
     <RadixSelect.Root onValueChange={(value: string) => handleSelection(value)} value={multiple ? multipleSelection : selection}>
-      <RadixSelect.Trigger className="w-fit inline-flex items-center justify-center rounded px-4 py-2 text-sm leading-none h-9 gap-1 bg-white shadow-md hover:bg-mauve-100 focus:outline-none focus:ring-2 focus:ring-black" aria-label="Food">
+      <RadixSelect.Trigger className="w-fit inline-flex items-center justify-center rounded px-4 py-2 text-sm leading-none h-9 gap-1 bg-white shadow-md hover:bg-mauve-100 focus:outline-none focus:ring-2 focus:ring-black" aria-label="Select">
         <RadixSelect.Value placeholder={ selection.length == 0 ? placeholder : selection.length > 32 ? selection.slice(0, 24) + "..." : selection} >
           {selection.length > 32 ? selection.slice(0, 24) + "..." : selection}
           {selection.length == 0 && placeholder}
